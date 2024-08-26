0b660f3fe97dd00cf17c9f086a1e78080e84f620
Rhannya Oliveira <112597314+Rhanny@users.noreply.github.com>
2024-08-26 19:08:17 -0300

    Update prob05.js

--- aula 06/prob05.js
+++ aula 06/prob05.js
@@ -1,15 +1,15 @@
 function verificarLimiteCaracteres(str) {
-    const limite = 20;
     const comprimento = str.length;
 
-    if (comprimento > limite) {
+    if (comprimento > 20) {
         console.log("O limite de caracteres foi excedido.");
-    } else if (comprimento === limite) {
+    } else if (comprimento === 20) {
         console.log("Você atingiu o limite de caracteres.");
     } else {
         console.log("O limite de caracteres ainda não foi excedido...");
     }
 }
 
-verificarLimiteCaracteres("Este é um exemplo de string"); 
-verificarLimiteCaracteres("Este é um exemplo de string com mais de vinte caracteres");
+verificarLimiteCaracteres("é um exemplo no limite");
+verificarLimiteCaracteres("Me chamo Rhannya ######")
+verificarLimiteCaracteres("Este é um exemplo de string com mais de vinte caracteres"); 
